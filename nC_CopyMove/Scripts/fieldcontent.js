7e12689950f91bc4b3ce4e8ee058df1025fdcfce
René Lange <reneolange@hotmail.com>
2016-08-08 11:19:59 +0200

    FieldContents2

---
+++ nC_CopyMove/Scripts/fieldcontent.js
@@ -0,0 +1,33 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var FieldContent;
+    return {
+        setters:[],
+        execute: function() {
+            class FieldContent {
+                constructor(value, field) {
+                    switch (field.type) {
+                        case "TaxonomyFieldTypeMulti":
+                            var terms = new Array();
+                            var termValues;
+                            for (var i = 0; i < value.get_count(); i++) {
+                                terms.push("-1;#" + value.getItemAtIndex(i).get_label() + "|" + value.getItemAtIndex(i).get_termGuid());
+                            }
+                            //Update
+                            this.value = terms.join(";#");
+                            this.field = field;
+                            break;
+                        case "Taxoother":
+                            break;
+                        default:
+                            this.value = value;
+                            this.field = field;
+                    }
+                }
+            }
+            exports_1("FieldContent", FieldContent);
+        }
+    }
+});
+//# sourceMappingURL=fieldcontent.js.map
---
+++ nC_CopyMove/app/fieldcontent.ts
@@ -0,0 +1,35 @@
+import {ListField} from './listFields'
+
+export class FieldContent{
+
+value: any;
+field: ListField;
+
+    constructor(value, field:ListField)
+    {
+        switch(field.type){
+            case "TaxonomyFieldTypeMulti":
+                                var terms = new Array();
+                                var termValues;
+                                for (var i = 0; i < value.get_count(); i++) {
+                                    terms.push("-1;#" + value.getItemAtIndex(i).get_label() + "|" + value.getItemAtIndex(i).get_termGuid());
+                                }
+                                //Update
+
+                                this.value= terms.join(";#");
+                                this.field = field;
+               break;
+
+            case "Taxoother":
+                break;
+
+            default:
+                this.value = value;
+                this.field = field;
+
+        }
+
+
+        }
+        
+}
